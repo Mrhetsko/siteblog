71d15e573f2b6f8cc3fcedc57f0f09ea622d233f
Dmytro Hetsko <49610661+Mrhetsko@users.noreply.github.com>
2022-08-27 21:53:16 +0200

    Initial

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    # path('', views.index, name='home'),
+    path('', views.Home.as_view(), name='home'),
     path('category/<str:slug>/', views.get_category, name='category'),
+    path('post/<str:slug>/', views.get_post, name="post")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Post, Category, Tag
+
+
+class Home(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Classic blog design'
+        return context
 
 
 def index(request):
@@ -7,4 +20,8 @@ def index(request):
 
 
 def get_category(request, slug):
+    return render(request, 'blog/index.html')
+
+
+def get_post(request, slug):
     return render(request, 'blog/index.html')
